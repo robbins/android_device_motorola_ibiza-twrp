da5e26e188f2bc74f8c75738918007fbbd9b9d9e
Nathanael Robbins <nejrobbins@gmail.com>
2023-07-22 20:18:31 -0400

    Add DTB & DTBO images, and restructure tree
    DTB is from vendor_boot image, restructure followed TeamWin/android_device_motorola_hanoip.

--- device.mk
+++ device.mk
@@ -1,6 +1,14 @@
-# TWRP
-TARGET_SCREEN_WIDTH := 720
-TARGET_SCREEN_HEIGHT := 1600
+LOCAL_PATH := device/motorola/ibiza
+
+# Enable virtual A/B OTA
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
+PRODUCT_PLATFORM := sm4350
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
 
 # A/B
 AB_OTA_UPDATER := true
@@ -17,3 +25,7 @@ AB_OTA_PARTITIONS += \
 
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# TWRP
+TARGET_SCREEN_WIDTH := 720
+TARGET_SCREEN_HEIGHT := 1600

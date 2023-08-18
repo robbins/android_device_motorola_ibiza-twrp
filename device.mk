a46ee6815c9f5dd289c84747d3d7ba1f5eebe6ed
Nathanael Robbins <nejrobbins@gmail.com>
2023-08-18 19:26:43 -0400

    Add a bunch more libraries from stock

--- device.mk
+++ device.mk
@@ -6,10 +6,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 PRODUCT_PLATFORM := sm4350
 
 PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd \
     qcom_decrypt \
-    qcom_decrypt_fbe
+    qcom_decrypt_fbe \
+    android.hardware.boot@1.1-impl-qti.recovery \
+    bootctrl.recovery
 
 # A/B
 AB_OTA_UPDATER := true
@@ -30,3 +33,7 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # TWRP
 TARGET_SCREEN_WIDTH := 720
 TARGET_SCREEN_HEIGHT := 1600
+
+PRODUCT_SHIPPING_API_LEVEL := 30
+
+PRODUCT_TARGET_VNDK_VERSION := 30
